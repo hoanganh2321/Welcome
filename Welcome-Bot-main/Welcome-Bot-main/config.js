0b3473ce02bdbbd35bb784c2beb553f947cee82c
hoanganh2321 <131464214+hoanganh2321@users.noreply.github.com>
2024-04-18 19:01:56 +0700

    Add files via upload

---
+++ Welcome-Bot-main/Welcome-Bot-main/config.js
@@ -0,0 +1,32 @@
+
+
+module.exports = {
+  TOKEN: "MTIyNTQyOTY0Njg1NDU5MDUwNg.GNbgQH.-DdhcvYWudExnNxEJYkcOggjPaKMED2gR6McUs",
+  ownerID: ["779507251282968587", ""], 
+  commandsDir: './commands',  
+  embedColor: "00fbff",
+  errorLog: "1227549996988235826", 
+
+}
+
+/*
+
+  ________.__                        _____.___.___________
+ /  _____/|  | _____    ____  ____   \__  |   |\__    ___/
+/   \  ___|  | \__  \ _/ ___\/ __ \   /   |   |  |    |   
+\    \_\  \  |__/ __ \\  \__\  ___/   \____   |  |    |   
+ \______  /____(____  /\___  >___  >  / ______|  |____|   
+        \/          \/     \/    \/   \/                  
+
+╔════════════════════════════════════════════════════════════════════════╗
+║                                                                        ║
+║  ## Created by GlaceYT!                                                ║
+║  ## Feel free to utilize any portion of the code                       ║
+║  ## DISCORD :  https://discord.com/invite/xQF9f9yUEM                   ║
+║  ## YouTube : https://www.youtube.com/@GlaceYt                         ║
+║                                                                        ║
+╚════════════════════════════════════════════════════════════════════════╝
+
+
+*/
+
